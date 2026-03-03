1e1a9c5cd072ad229baf2eb92301188ecd9bf9cf
adrianreigfernandez-oss <reigfernandezadrian@gmail.com>
2026-03-03 19:58:01 +0100

    Deveres hechos json 1

--- src/datos.py
+++ src/datos.py
@@ -1,3 +1,47 @@
 import os
 import json
 import yfinance as yf
+from config import ACCESS_TOKEN
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+#Obtener tickers del sp500 ( las mejores empresas de eeuu, hay 500 pero usaremos estas 5 que son las mas importantes)
+tickers = ["AAPL", "NVDA", "GOOGL", "AMZN", "MSFT"]
+
+data_sp500 = {}
+
+#with open():
+for ticker in tickers:
+    try:
+        accion = yf.download(ticker, start='2020-01-01', end='2021-01-01')
+        accion_json = accion.to_json(orient='records')
+        sp500 = json.loads(accion_json)
+        if sp500:
+            data_sp500[ticker] = sp500
+        else:
+            print("Error")
+
+            
+
+
+    
+    except Exception as e:
+        print(f"Se a producido un error en {ticker}: {e}")
+
+with open("data/json_sp500.json", "w", encoding= "utf-8") as bolsa500:
+    json.dump(data_sp500,bolsa500,indent=4,ensure_ascii=False)
+
+
+
+
+
+
+
+
+
+
+
