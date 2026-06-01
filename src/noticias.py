7a6072d1bd0208a2f1f4983fdccc9114f9f58c18
adrianreigfernandez-oss <reigfernandezadrian@gmail.com>
2026-06-01 18:14:15 +0200

    areglo

---
+++ src/noticias.py
@@ -0,0 +1,27 @@
+import os
+import json
+import yfinance as yf
+from config import ACCESS_TOKEN
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from datetime import datetime
+from datos_ibex35 import tickers as tikers1
+from datos_sp500 import tickers as tikers2
+todotikers = tikers1 + tikers2
+try:
+    for accion in todotikers:
+        # Obtener noticias de accion
+        ticker_obj = yf.Ticker(accion)
+        noticias_completas = ticker_obj.news
+        noticias_recientes = noticias_completas[:1]
+
+        with open("data/json/news.json", "a", encoding= "utf-8") as noticias:
+            json.dump(noticias_recientes,noticias,indent=4,ensure_ascii=False)
+
+except Exception as e:
+    print(f"Error en {todotikers}: {e}")
+
+
+
